76c3d1ec509db15caa6713e3b8b0d02d65272407
Morten Lied Johansen <mortenjo@ifi.uio.no>
2018-11-20 22:39:53 +0100

    Ensure all models used in fields are rendered before models that needs them

--- bin/generator.py
+++ bin/generator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8
 
 import os
-from collections import namedtuple, defaultdict
+from collections import namedtuple
 from pprint import pprint
 
 import appdirs
@@ -22,7 +22,7 @@ TYPE_MAPPING = {
     "array": "list",
     "map": "dict",
     "boolean": "bool",
-    "string": "str",
+    "string": "six.text_type",
     "date": "date",
     "DateTime": "datetime",
     "object": "dict",
@@ -34,102 +34,205 @@ TYPE_MAPPING = {
 
 GVK = namedtuple("GVK", ("group", "version", "kind"))
 Field = namedtuple("Field", ("name", "description", "type", "ref"))
-Definition = namedtuple("Definition", ("package", "name", "description", "fields", "gvks"))
-Operation = namedtuple("Operation", ("path", "action", "description"))
-Model = namedtuple("Model", ("definition", "operations"))
-Import = namedtuple("Import", ("package", "name"))
+Definition = namedtuple("Definition", ("name", "description", "fields", "gvks"))
 
 
-def _parse_definitions(definitions):
-    result = {}
-    for id, item in definitions.items():
-        package, name = id[len("io.k8s."):].rsplit(".", 1)
-        package = package.replace("-", "_")
-        gvks = [GVK(**x) for x in item.get("x-kubernetes-group-version-kind", [])]
-        fields = []
-        for field_name, property in item.get("properties", {}).items():
-            fields.append(Field(field_name, property.get("description", ""), property.get("type"), property.get("$ref")))
-        definition = Definition(package, name, item.get("description", ""), fields, gvks)
-        key = (package, name)
-        result[key] = definition
-    print("Extracted {} definitions in total".format(len(result)))
-    return result
+# Operation = namedtuple("Operation", ("path", "action", "description"))
+# Model = namedtuple("Model", ("definition", "operations"))
 
 
-def _parse_paths(paths):
-    result = defaultdict(list)
-    for id, item in paths.items():
-        methods = (p for p in item.keys() if p not in ("parameters", "$ref"))
-        for method in methods:
-            operation = item[method]
-            gvks = operation.get("x-kubernetes-group-version-kind")
-            if not gvks:
-                continue
-            key = GVK(**gvks)
-            action = operation.get("x-kubernetes-action")
-            if not action:
-                continue
-            o = Operation(id, action, operation.get("description", ""))
-            result[key].append(o)
-    total = sum(len(x) for x in result.values())
-    print("Extracted {} gvks, with {} operations in total".format(len(result), total))
-    return result
+class Primitive(object):
+    def __init__(self, type):
+        self.name = TYPE_MAPPING.get(type, type)
 
 
-def _make_models(definitions, operations):
-    result = defaultdict(list)
-    for key in definitions.keys():
-        package, name = key
-        print("Generating model for {}.{}, based on this definition:".format(package, name))
-        definition = definitions[key]
-        pprint(definition)
-        result[definition.package].append(Model(definition, None))
-    print("Collected {} models in total".format(len(result)))
-    return result
+class Child(object):
+    @property
+    def parent_ref(self):
+        return self.ref.rsplit(".", 1)[0]
 
 
-def _generate_package(package, models, imports, output_dir):
-    env = Environment(
-        loader=FileSystemLoader(os.path.dirname(__file__)),
-        trim_blocks=True,
-        lstrip_blocks=True,
-    )
-    file_path = os.path.join(output_dir, package.replace(".", os.sep)) + ".py"
-    package_dir = os.path.dirname(file_path)
-    if not os.path.isdir(package_dir):
-        os.makedirs(package_dir)
-    for root, dirs, files in os.walk(output_dir):
-        if "__init__.py" in files:
-            continue
-        with open(os.path.join(root, "__init__.py"), "w") as fobj:
-            fobj.write("# Generated file")
-    template = env.get_template("model.jinja2")
-    with open(file_path, "w") as fobj:
-        fobj.write(template.render(models=models, imports=imports))
+class Module(namedtuple("Module", ("ref", "name", "imports", "models")), Child):
+    pass
 
 
-def _resolve_ref(ref):
-    return ref[len("#/definitions/io.k8s."):]
+class Model(namedtuple("Model", ("ref", "definition",)), Child):
+    @property
+    def name(self):
+        return self.definition.name
 
 
-def _resolve_types(package, models):
-    imports = set()
-    for model in models:
+class Package(namedtuple("Package", ("ref", "modules"))):
+    @property
+    def path(self):
+        return self.ref.replace(".", os.sep)
+
+
+class Import(namedtuple("Import", ("module", "models"))):
+    @property
+    def names(self):
+        return sorted(m.definition.name for m in self.models)
+
+
+class Parser(object):
+    def __init__(self, spec):
+        self._spec = spec.get("definitions", {})
+        self._packages = {}
+        self._modules = {}
+        self._models = {}
+
+    def parse(self):
+        self._parse_models()
+        self._resolve_references()
+        self._sort()
+        return self._packages.values()
+
+    def _parse_models(self):
+        for id, item in self._spec.items():
+            package_ref, module_name, def_name = _split_ref(id[len("io.k8s."):])
+            package = self._get_package(package_ref)
+            module = self._get_module(package, module_name)
+            gvks = [GVK(**x) for x in item.get("x-kubernetes-group-version-kind", [])]
+            fields = []
+            for field_name, property in item.get("properties", {}).items():
+                fields.append(
+                    Field(field_name, property.get("description", ""), property.get("type"), property.get("$ref")))
+            definition = Definition(def_name, item.get("description", ""), fields, gvks)
+            model = Model(_make_ref(package.ref, module.name, def_name), definition)
+            module.models.append(model)
+            self._models[model.ref] = model
+        print("Completed parse. Parsed {} packages, {} modules and {} models.".format(len(self._packages),
+                                                                                      len(self._modules),
+                                                                                      len(self._models)))
+
+    def _get_package(self, package_ref):
+        if package_ref not in self._packages:
+            package = Package(package_ref, [])
+            self._packages[package_ref] = package
+        return self._packages[package_ref]
+
+    def _get_module(self, package, module_name):
+        ref = _make_ref(package.ref, module_name)
+        if ref not in self._modules:
+            module = Module(ref, module_name, [], [])
+            package.modules.append(module)
+            self._modules[ref] = module
+        return self._modules[ref]
+
+    def _get_model(self, package, module, def_name):
+        ref = _make_ref(package.ref, module.name, def_name)
+        return self._models[ref]
+
+    def _resolve_references(self):
+        for module in self._modules.values():
+            imports = {}
+            for model in module.models:
+                self._resolve_fields(model.definition)
+                for field in model.definition.fields:
+                    ft = field.type
+                    if isinstance(ft, Model):
+                        if ft.parent_ref != model.parent_ref:
+                            if ft.parent_ref not in imports:
+                                package_ref, module_name, def_name = _split_ref(ft.ref)
+                                package = self._get_package(package_ref)
+                                ft_module = self._get_module(package, module_name)
+                                imports[ft.parent_ref] = Import(ft_module, [])
+                            imp = imports[ft.parent_ref]
+                            if ft not in imp.models:
+                                imp.models.append(ft)
+            module.imports[:] = imports.values()
+
+    def _resolve_fields(self, definition):
         new_fields = []
-        for field in model.definition.fields:
+        for field in definition.fields:
             if field.ref:
-                field_type = _resolve_ref(field.ref)
+                field_type = self._resolve_ref(field.ref)
             else:
-                field_type = TYPE_MAPPING.get(field.type, field.type)
-            if "." in field_type:
-                if not field_type.startswith(package):
-                    field_package, field_type = field_type.rsplit(".", 1)
-                    imports.add(Import(field_package, field_type))
-                else:
-                    field_type = field_type[len(package)+1:]
+                field_type = Primitive(field.type)
             new_fields.append(field._replace(type=field_type))
-        model.definition.fields[:] = new_fields
-    return imports
+        definition.fields[:] = new_fields
+
+    def _resolve_ref(self, ref):
+        package_ref, module_name, def_name = _split_ref(ref[len("#/definitions/io.k8s."):])
+        package = self._get_package(package_ref)
+        module = self._get_module(package, module_name)
+        model = self._get_model(package, module, def_name)
+        return model
+
+    def _sort(self):
+        for package in self._packages.values():
+            for module in package.modules:
+                module.imports.sort(key=lambda i: i.module.ref)
+                module.models[:] = self._sort_models(module.models)
+
+    def _sort_models(self, models):
+        """We need to make sure that any model that references an other model comes later in the list"""
+        Node = namedtuple("Node", ("model", "dependants", "dependencies"))
+        nodes = {}
+        for model in models:
+            node = nodes.setdefault(model.ref, Node(model, [], []))
+            for field in model.definition.fields:
+                if isinstance(field.type, Model) and field.type.parent_ref == model.parent_ref:
+                    dep = nodes.setdefault(field.type.ref, Node(field.type, [], []))
+                    node.dependencies.append(dep)
+        for node in nodes.values():
+            for dep in node.dependencies:
+                dep.dependants.append(node)
+        top_nodes = [n for n in nodes.values() if len(n.dependencies) == 0]
+        models = []
+        while top_nodes:
+            top_node = top_nodes.pop()
+            models.append(top_node.model)
+            for dep in top_node.dependants:
+                dep.dependencies.remove(top_node)
+                if len(dep.dependencies) == 0:
+                    top_nodes.append(dep)
+        return models
+
+
+class Generator(object):
+    def __init__(self, packages, output_dir):
+        self._packages = packages
+        self._output_dir = output_dir
+        self._env = Environment(
+            loader=FileSystemLoader(os.path.dirname(__file__)),
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+
+    def generate(self):
+        print("Generating models in {}".format(self._output_dir))
+        for package in self._packages:
+            self._generate_package(package)
+        for root, dirs, files in os.walk(self._output_dir):
+            if "__init__.py" in files:
+                continue
+            with open(os.path.join(root, "__init__.py"), "w") as fobj:
+                fobj.write("# Generated file")
+
+    def _generate_package(self, package):
+        package_dir = os.path.join(self._output_dir, package.path)
+        if not os.path.isdir(package_dir):
+            os.makedirs(package_dir)
+        print("Created package {}.".format(package.ref))
+        for module in package.modules:
+            self._generate_module(module, package_dir)
+
+    def _generate_module(self, module, package_dir):
+        template = self._env.get_template("model.jinja2")
+        module_path = os.path.join(package_dir, module.name) + ".py"
+        with open(module_path, "w") as fobj:
+            fobj.write(template.render(module=module))
+        print("Generated module {}.".format(module.ref))
+
+
+def _split_ref(s):
+    s = s.replace("-", "_")
+    return s.rsplit(".", 2)
+
+
+def _make_ref(*args):
+    return ".".join(args)
 
 
 def main():
@@ -138,13 +241,11 @@ def main():
     for key in ("paths", "definitions", "parameters", "responses", "securityDefinitions", "security", "tags"):
         print("Specification contains {} {}".format(len(spec.get(key, [])), key))
     pprint(spec["info"])
-    definitions = _parse_definitions(spec["definitions"])
-    #operations = _parse_paths(spec["paths"])
-    models = _make_models(definitions, None)
-    for package, models in models.items():
-        imports = _resolve_types(package, models)
-        _generate_package(package, models, imports,
-                          os.path.join(os.path.dirname(os.path.dirname(__file__)), "k8s", "models"))
+    output_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "k8s", "models")
+    parser = Parser(spec)
+    packages = parser.parse()
+    generator = Generator(packages, output_dir)
+    generator.generate()
 
 
 if __name__ == "__main__":
