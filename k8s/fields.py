aa97525ee1101dc4e98218c2489d1baa300539d4
Thomas Geirhovd <tg90nor@gmail.com>
2021-12-07 16:08:54 +0100

    Fix pylint issues

--- k8s/fields.py
+++ k8s/fields.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8
 
 # Copyright 2017-2019 The FIAAS Authors
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -188,7 +188,7 @@ class JSONField(Field):
     def _check_allowed_types(self, value, chain=None):
         if chain is None:
             chain = []
-        if type(value) in self.allowed_types:
+        if any(isinstance(value, t) for t in self.allowed_types):
             if isinstance(value, dict):
                 for k, v in value.items():
                     self._check_allowed_types(k, chain.append(k))
