5baf84161ab1b7e9cb3809e2fb063d247880d587
Jesper Ekhall <jesper@blocket.se>
2024-04-26 15:57:13 +0200

    Add Python 3.12 as well.

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ def main():
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Topic :: Internet",
             "Topic :: Software Development :: Libraries :: Python Modules",
             "Topic :: System :: Clustering",
