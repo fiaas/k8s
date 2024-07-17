cd61e9ae62467196c089a52ca8d3bfdd9d1a26db
Martin Kulich <kulda.zbraslav@gmail.com>
2024-07-17 09:00:53 +0200

    Add publishNotReadyAddresses to ServiceSpec

--- k8s/models/service.py
+++ k8s/models/service.py
@@ -38,6 +38,7 @@ class ServiceSpec(Model):
     sessionAffinity = Field(str, "None")
     loadBalancerSourceRanges = ListField(str)
     externalTrafficPolicy = Field(str, "None")
+    publishNotReadyAddresses = Field(bool)
 
 
 class Service(Model):
